ac09b3bd1b4deb137cf9f7de154753efe3ea97b7
sjr <shanjr2000@gmail.com>
2026-02-18 14:35:07 +0800

    完成lab2

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,6 +126,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p->kama_syscall_trace=0;
 
   return p;
 }
@@ -296,6 +297,8 @@ fork(void)
   np->state = RUNNABLE;
 
   release(&np->lock);
+
+  np->kama_syscall_trace=p->kama_syscall_trace;
 
   return pid;
 }
@@ -692,4 +695,13 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+void get_procnum(uint64 * dst){
+	*dst=0;
+	struct proc* p;
+	for(p=proc;p<&proc[NPROC];p++){
+		if(p->state!=UNUSED)
+			(*dst)++;
+	}
 }
